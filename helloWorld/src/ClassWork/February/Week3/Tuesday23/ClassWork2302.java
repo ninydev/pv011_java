59d18fe9e8376531a0028e882d267f12ada54fcb
Oleksandr Nykytin <keeper@ninydev.com>
2023-02-23 19:38:07 +0200

    Connect to MySql

--- helloWorld/src/ClassWork/February/Week3/Tuesday23/ClassWork2302.java
+++ helloWorld/src/ClassWork/February/Week3/Tuesday23/ClassWork2302.java
@@ -18,7 +18,25 @@ public class ClassWork2302 implements Runnable
                     "jdbc:mysql://localhost:52000/pv011",
                     "root", "mypassword");
 
+            System.out.println("DB Ok");
+
+            String sqlQuery = "CREATE TABLE posts (" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "title VARCHAR (64)" +
+                    ")";
+
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sqlQuery);
+            // executeUpdate -- INSERT, UPDATE, CREATE ....
+            // executeQuery -- SELECT
+            // execute -- true (если есть ответ от SELECT) false
+
+
+
+            conn.close();
+
         } catch (Exception e) {
+            System.out.println("DB Error " + e.getMessage());
 
         }
 
