f4f2080ffb1f16267f02ff8f017246c850408840
Oleksandr Nykytin <keeper@ninydev.com>
2023-03-01 19:25:54 +0200

    Counties to Cities

--- helloWorld/src/ClassWork/February/Week4/Wednsday01/Models/City.java
+++ helloWorld/src/ClassWork/February/Week4/Wednsday01/Models/City.java
@@ -3,17 +3,17 @@ package ClassWork.February.Week4.Wednsday01.Models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="cities")
+// @Table(name="cities")
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    // @Column(name="id")
     private int id;
 
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "country_id")
+    // @JoinColumn(name = "country_id")
     private Country country;
 
 }
--- helloWorld/src/ClassWork/February/Week4/Wednsday01/Models/Country.java
+++ helloWorld/src/ClassWork/February/Week4/Wednsday01/Models/Country.java
@@ -5,11 +5,11 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name="countries")
+// @Table(name="countries")
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    // @Column(name="id")
     private int id;
 
     private String name;
@@ -18,7 +18,7 @@ public class Country {
     private Set<City> cities;
 
     @OneToOne
-    @JoinColumn(name = "catipal_city_id")
+    // @JoinColumn(name = "catipal_city_id")
     private City capital;
 
 
